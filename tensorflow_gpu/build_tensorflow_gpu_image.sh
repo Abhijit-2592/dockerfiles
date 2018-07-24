6cbf4356e5c113329f3e96de7b21890d27946557
Abhijit-2592 <balaabhijit5@gmail.com>
2018-07-24 22:11:22 +0530

    downgrade cudnn version

--- tensorflow_gpu/build_tensorflow_gpu_image.sh
+++ tensorflow_gpu/build_tensorflow_gpu_image.sh
@@ -2,6 +2,6 @@
 # @Author: abhijit
 # @Date:   24-May-2018 16-05-47
 # @Last modified by:   abhijit
-# @Last modified time: 24-May-2018 16-05-65
+# @Last modified time: 2018-07-02T18:22:28+05:30
 
-docker build -t tensorflow_gpu_v1 .
+docker build -t tensorflow_gpu_v2 .
